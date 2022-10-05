0ae4565067565dff503b6dc57d17ffb9de59ab46
DKapustin <32310510+DKapustin@users.noreply.github.com>
2022-10-05 21:11:18 +0300

    hometask-2

---
+++ core/src/test/java/liga/medical/personservice/core/PersonServiceApplicationTest.java
@@ -0,0 +1,11 @@
+package liga.medical.personservice.core;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class PersonServiceApplicationTest {
+    @Test
+    public void contextLoads() {
+    }
+}
